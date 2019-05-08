877dc279c00df20357c4619a89237e7de926de1a
Robert Albus <hello@robertalbus.com>
2019-05-08 17:14:39 -0600

    Create user input service

---
+++ src/ui-module/input.service.ts
@@ -0,0 +1,54 @@
+import { StateMachine } from "state-module/state-machine";
+
+export class InputService {
+
+  private rl = require('readline-sync')
+  private inputValidator = new RegExp('([0-9AKQJT]{5})( )([0-9AKQJT]{5})', 'i')
+  
+
+  constructor(private state: StateMachine) {
+  }
+
+  public getUserInput() {
+    let quantity = this.getNumberOfRounds();
+    let rounds = this.getRounds(quantity);
+    this.save(rounds);
+  }
+
+  private save(data: string[]) {
+    this.state.save(data)
+  }
+  
+  private getNumberOfRounds():number {
+    let response = this.rl.questionInt('How many pairs of hands? ');
+    
+    if (response > 0) {
+      return response;
+    } 
+    if (response === 0 ) {
+      process.exit()
+    }
+    
+    console.log('Please enter a positive number or 0 to exit');
+    this.getNumberOfRounds()
+  }
+
+  private getRounds(quantity: number): string[] {
+    let hands: string[] = [];
+
+    for (let i = 0; i < quantity; ++i) {
+      hands.push(this.getRound())
+    }
+    return hands
+  }
+
+  private getRound(): string {
+    let response = this.rl.question('=> ');
+    if (this.inputValidator.test(response)) {
+      return response;
+    }
+    console.log('Invalid. Please try again with valid cards: 0-9,TJQKA');
+    return this.getRound();
+  }
+
+}
