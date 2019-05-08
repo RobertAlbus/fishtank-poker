557a20f3ac78b352eb7cb8ef4eb384045f0c94d1
Robert Albus <hello@robertalbus.com>
2019-05-08 17:11:51 -0600

    Create types

---
+++ src/types/hand.enum.ts
@@ -0,0 +1,17 @@
+export enum handEnum {
+  'HIGHCARD'      = 0,
+  'PAIR'          = 1,
+  'TWOPAIR'       = 2,
+  'THREEOFAKIND'  = 3,
+  'STRAIGHT'      = 4,
+  'FULLHOUSE'     = 5,
+  'FOUROFAKIND'   = 6,
+}
+
+export enum cardEnum {
+  'T' = 10,
+  'J' = 11,
+  'Q' = 12,
+  'K' = 13,
+  'A' = 14
+}
---
+++ src/types/histogram.ts
@@ -0,0 +1,8 @@
+export class HistogramItem {
+    constructor(
+      public value: number,
+      public quantity: number
+    ){}
+  }
+  export type Histogram = HistogramItem[]
+  
