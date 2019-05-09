6b408d2268de25015fda166022d13c895372e595
Robert Albus <hello@robertalbus.com>
2019-05-08 18:04:07 -0600

    Create preprocessor class with DI for state

--- src/hand-evaluator/preprocessor_v2.ts
+++ src/hand-evaluator/preprocessor_v2.ts
@@ -1,67 +1,81 @@
 import { cardEnum } from "../types/hand.enum";
 import { Histogram, HistogramItem } from '../types/histogram'
+import { StateMachine } from "state-module/state-machine";
 
-function batchStringToHistograms(strings: string[]): Histogram[][] {
-  let histograms: Histogram[][] = [];
+export class Preprocessor {
 
-  strings.map( (string, i) => {
-    histograms.push(stringToHistograms(string));
-  })
+  constructor(private state: StateMachine) {
 
-  return histograms;
-}
+  }
 
-function stringToHistograms(input: string): Histogram[] {
-  // discreet hands from one string
-  let hands = stringToHands(input);
-  let histograms: Histogram[] = []
+  processAll() {
+    this.state.histograms = 
+    this.batchStringToHistograms(this.state.strings)
+  }
 
-  hands.map (hand => {
-    let cards = handToCards(hand);
-    let numeric = cardsToNumeric(cards);
-    let histogram = numericToHistogram(numeric);
+  batchStringToHistograms(strings: string[]): Histogram[][] {
+    let histograms: Histogram[][] = [];
+  
+    strings.map( (string, i) => {
+      histograms.push(this.stringToHistograms(string));
+    })
+  
+    return histograms;
+  }
 
-    histograms.push(histogram)
-  })
+  stringToHistograms(input: string): Histogram[] {
+    // discreet hands from one string
+    let hands = this.stringToHands(input);
+    let histograms: Histogram[] = []
+  
+    hands.map (hand => {
+      let cards = this.handToCards(hand);
+      let numeric = this.cardsToNumeric(cards);
+      let histogram = this.numericToHistogram(numeric);
+  
+      histograms.push(histogram)
+    })
+  
+    return histograms
+  }
 
-  return histograms
-}
+  ////////
+  // TRANSFORMATIONS
 
-////////
-// TRANSFORMATIONS
+  stringToHands(input: string): string[] {
+    return input.split(' ');
+  }
 
-function stringToHands(input: string): string[] {
-  return input.split(' ');
-}
-
-function handToCards(hand: string): string[] {
-  return hand.split('')
-}
-
-function cardsToNumeric(hand: string[]): number[] {
-  let numeric: number[] = [];
-  hand.map( card => {
-    numeric.push(parseInt(card) || cardEnum[card])
-  })
-  return numeric;
-}
-
-function numericToHistogram(hand: number[]): Histogram {
-  let histogram: Histogram = []
-
-  while(hand.length > 0) {
-    let value: number = hand[0];
-    let count: number = 0;
-    for (let card of hand) {
-      if (card === value) {
-        ++count
-      }
-    }
-    let hi = new HistogramItem(value, count)
-    // console.log(hi.value, hi.quantity)
-    histogram.push(new HistogramItem(value, count));
-    hand = hand.filter( c => c !== value);
+  handToCards(hand: string): string[] {
+    return hand.split('')
   }
   
-  return histogram
+  cardsToNumeric(hand: string[]): number[] {
+    let numeric: number[] = [];
+    hand.map( card => {
+      numeric.push(parseInt(card) || cardEnum[card])
+    })
+    return numeric;
+  }
+
+  numericToHistogram(hand: number[]): Histogram {
+    let histogram: Histogram = []
+  
+    while(hand.length > 0) {
+      let value: number = hand[0];
+      let count: number = 0;
+      for (let card of hand) {
+        if (card === value) {
+          ++count
+        }
+      }
+      let hi = new HistogramItem(value, count)
+      console.log(hi.value, hi.quantity)
+      histogram.push(new HistogramItem(value, count));
+      hand = hand.filter( c => c !== value);
+    }
+    
+    return histogram
+  }
+
 }
