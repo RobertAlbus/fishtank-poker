93dc8dd11e3eedd8543073749c22d1e1a874d6d6
Robert Albus <hello@robertalbus.com>
2019-05-08 18:04:25 -0600

    Initial commit

---
+++ src/poker.ts
@@ -0,0 +1,22 @@
+import { StateMachine } from './state-module/state-machine';
+import { InputService } from './ui-module/input.service';
+import { HandEvaluator } from './hand-evaluator/hand-evaluator';
+import { OutputService } from './ui-module/output.service';
+import { Preprocessor } from './hand-evaluator/preprocessor_v2';
+
+const state         = new StateMachine();
+
+const input         = new InputService(state);
+const preprocessor  = new Preprocessor(state);
+const evaluator     = new HandEvaluator(state);
+const output        = new OutputService(state);
+
+//input.getUserInput();
+
+let mockdata = 'TTT99 23456'
+input.programmaticInput([mockdata, mockdata])
+preprocessor.processAll();
+evaluator.computeWinners();
+
+
+output.display();
