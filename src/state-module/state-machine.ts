fa140b480c2ff28805770a1b201bd52dac5870a4
Robert Albus <hello@robertalbus.com>
2019-05-09 01:54:04 -0600

    Add winners state tracking

--- src/state-module/state-machine.ts
+++ src/state-module/state-machine.ts
@@ -3,10 +3,12 @@ import { Histogram } from "types/histogram";
 export class StateMachine {
   strings: string[];
   histograms: Histogram[][];
+  winners: string[];
 
   constructor() {
-    this.strings = []
-    this.histograms = []
+    this.strings    = [];
+    this.histograms = [];
+    this.winners    = [];
   }
 
 }
