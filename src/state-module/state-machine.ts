90c5ac10f5af33e530fdbd0ba6417963540918ed
Robert Albus <hello@robertalbus.com>
2019-05-10 10:42:10 -0600

    Make initialization callable

--- src/state-module/state-machine.ts
+++ src/state-module/state-machine.ts
@@ -2,16 +2,19 @@ import { Histogram } from "types/histogram";
 import { handEnum } from "types/hand.enum";
 
 export class StateMachine {
-  strings: string[];
-  histograms: Histogram[][];
-  enums: handEnum[][];
-  winnerStrings: string[];
+  strings       : string[];
+  histograms    : Histogram[][];
+  enums         : handEnum[][];
+  winnerStrings : string[];
 
   constructor() {
-    this.strings        = [];
-    this.histograms     = [];
-    this.enums          = []
-    this.winnerStrings  = [];
+    this.init();
   }
 
+  init() {
+    this.strings        = [];
+    this.histograms     = [];
+    this.enums          = [];
+    this.winnerStrings  = [];
+  }
 }
