035502c2bb41bf095113a853da0ee1b1be1b28ee
Robert Albus <hello@robertalbus.com>
2019-05-10 00:14:44 -0600

    Track new piece of state

--- src/state-module/state-machine.ts
+++ src/state-module/state-machine.ts
@@ -1,14 +1,17 @@
 import { Histogram } from "types/histogram";
+import { handEnum } from "types/hand.enum";
 
 export class StateMachine {
   strings: string[];
   histograms: Histogram[][];
-  winners: string[];
+  enums: handEnum[][];
+  winnerStrings: string[];
 
   constructor() {
-    this.strings    = [];
-    this.histograms = [];
-    this.winners    = [];
+    this.strings        = [];
+    this.histograms     = [];
+    this.enums          = []
+    this.winnerStrings  = [];
   }
 
 }
