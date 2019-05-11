2eef4fa1acb8ffa05917ad755b6d6910fd336fbe
Robert Albus <hello@robertalbus.com>
2019-05-11 15:44:39 -0600

    Add import for input service mock

--- src/poker.ts
+++ src/poker.ts
@@ -3,6 +3,7 @@ import { InputService } from './ui-module/input.service';
 import { HandEvaluator } from './hand-evaluator/hand-evaluator';
 import { OutputService } from './ui-module/output.service';
 import { Preprocessor } from './preprocessor/preprocessor_v2';
+import { InputServiceMock } from './ui-module/input.mock';
 
 const state         = new StateMachine();
 
