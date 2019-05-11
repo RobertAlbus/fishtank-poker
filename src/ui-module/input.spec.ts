1097a5a6078209ddc8981719c16d3ed941f9a2ea
Robert Albus <hello@robertalbus.com>
2019-05-11 16:19:17 -0600

    Write test for input upcasing

--- src/ui-module/input.spec.ts
+++ src/ui-module/input.spec.ts
@@ -10,9 +10,23 @@ describe('input service', () => {
     expect(input).toBeTruthy()
   })
   it('should save data to state machine', () => {
+
+    state.init();
+    
     let roundOfCards = "23456 99988"
     input.getUserInput([roundOfCards])
     expect(state.strings).toEqual([roundOfCards])
+    state.init();
+  })
+  it('should upcase any valid input', () => {
+    state.init();
+
+    console.log(state.strings)
+    input.getUserInput(['tjqka tttaa']);
+    console.log(state.strings)
+    expect(state.strings[0]).toEqual('TJQKA TTTAA')
+
+    state.init()
   })
 
 })
