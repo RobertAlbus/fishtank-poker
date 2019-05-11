00819593c24cfc8d67e03f567e1015f2fd1e40a3
Robert Albus <hello@robertalbus.com>
2019-05-10 18:31:14 -0600

    Write tests for input service

---
+++ src/ui-module/input.spec.ts
@@ -0,0 +1,18 @@
+import { StateMachine } from "../state-module/state-machine";
+import { InputService } from "./input.service";
+
+describe('input service', () => {
+
+  const state = new StateMachine();
+  const input = new InputService(state);
+
+  it('should create', () => {
+    expect(input).toBeTruthy()
+  })
+  it('should save data to state machine', () => {
+    let roundOfCards = "23456 99988"
+    input.getUserInput([roundOfCards])
+    expect(state.strings === [roundOfCards]).toBeTruthy()
+  })
+
+})
