3c8827eb4298cde9b9e4de767eb2ecde6090331b
Robert Albus <hello@robertalbus.com>
2019-05-11 15:39:59 -0600

    Add mock data loading functions

--- src/state-module/state-machine.mock.ts
+++ src/state-module/state-machine.mock.ts
@@ -1,6 +1,7 @@
 import { Histogram } from "../types/histogram";
 import { handEnum } from "../types/hand.enum";
-import * as data from '../mock-data/mock.data'
+import * as data from '../mock-data/mock.data';
+
 export class StateMachineMock {
   strings       : string[];
   histograms    : Histogram[][];
@@ -10,15 +11,35 @@ export class StateMachineMock {
   constructor() {
     this.init()
   }
+
   init() {
     this.strings        = [];
     this.histograms     = [];
     this.enums          = [];
     this.winnerStrings  = [];
   }
+
   load(...members: string[]) {
     members.map( member => {
       this[member] = data[member];
     })
+  }
+  loadAll() {
+    this.loadStrings();
+    this.loadHistograms();
+    this.loadEnums();
+    this.loadWinnerStrings()
+  }
+  loadStrings(){
+    this.strings = data.strings;
+  }
+  loadHistograms(){
+    this.histograms = data.histograms;
+  }
+  loadEnums(){
+    this.enums = data.enums;
+  }
+  loadWinnerStrings(){
+    this.winnerStrings = data.winnerStrings;
   }
 }
